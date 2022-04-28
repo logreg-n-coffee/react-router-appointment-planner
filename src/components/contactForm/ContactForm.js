14f951bd4b173711a22e1e7623de4f99ef086e76
Rui Hu <8199515+logreg-n-coffee@users.noreply.github.com>
2022-04-27 23:51:25 -0700

    completed functionalities

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// ContactForm takes in props passed in
+
 export const ContactForm = ({
   name,
   setName,
@@ -10,6 +12,32 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit} >
+      <input 
+        value={name}
+        type="text"
+        placeholder="Name"
+        onChange={({ target }) => setName(target.value)}
+        required
+      />
+      <input 
+        value={phone}
+        type="tel"
+        placeholder="Phone Number"
+        pattern="^[1-9][0-9]{2}[1-9][0-9]{2}[0-9]{4}$"
+        onChange={({ target }) => setPhone(target.value)}
+        required
+      />
+      <input 
+        value={email}
+        type="email"
+        placeholder="Email Address"
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <input 
+        type="submit"
+        
+      />
+    </form>
   );
 };
